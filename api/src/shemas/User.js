f22834a202d09cc542465f77081dff23793459c3
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-07-16 19:29:44 -0300

    feat: shemas com mongoose

---
+++ api/src/shemas/User.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const userShema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+  
+    });
+
+    export default model('users', userShema);
