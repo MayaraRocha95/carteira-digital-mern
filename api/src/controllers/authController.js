84dfb2e99df9afea891763f5c72d0979d8e99609
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-07-16 20:02:54 -0300

    feat: tratativas de rros mo controller

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -2,10 +2,15 @@ import authService from "../services/authService.js";
 
 async function signup(req, res) {
   const body = req.body;
- console.log(body);
+  try{
   const resService = await authService.signup(body);
+  res.status(201).send(resService);
 
-  res.send(resService);
+  } catch (error) {
+    res.status(409).send(error.message);
+  }
+
+
 }
 
 export default {
