e41c31702c93bba098a0a0db86c6e42c444d9db4
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-07-19 13:17:28 -0300

    validação de shemas com Joi

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import authController from '../controllers/authController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { validationSchemaMiddleware } from '../middlewares/validationSchemaMiddleware.js';
+import { CreateUser } from '../shemas/validation/CreateUser.js';
+import { AuthUser } from '../shemas/validation/AuthUser.js';
 
 const authRouter = Router();
 
-authRouter.post('/signup', authController.signup);
-authRouter.post('/signin', authController.signin);
+authRouter.post('/signup', validationSchemaMiddleware(CreateUser), authController.signup);
+authRouter.post('/signin', validationSchemaMiddleware(AuthUser), authController.signin);
 authRouter.get('/me', authMiddleware ,authController.userLogged);
 
 export default authRouter;
--- api/src/routes/transactionRoutes.js
+++ api/src/routes/transactionRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import transactionController from '../controllers/transactionController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { validationSchemaMiddleware } from '../middlewares/validationSchemaMiddleware.js';
+import { CreateTransaction } from '../shemas/validation/CreateTransaction.js';
 
 const transactionRouter = Router();
 
-transactionRouter.post('/transactions', authMiddleware,transactionController.create);
+transactionRouter.post('/transactions', authMiddleware,validationSchemaMiddleware(CreateTransaction),transactionController.create);
 
 transactionRouter.get('/transactions', authMiddleware,transactionController.findAllByUser);
 
